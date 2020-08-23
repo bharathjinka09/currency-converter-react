0f80d2be796d710208e225df3f401abad8b50ff9
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-23 13:52:49 +0530

    Updates

---
+++ precache-manifest.ded8ffa294c01d3c5a9d5bc28486ac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f283405ea9d4d1374ae52bfa5c748502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7a733d0f1dad4faa8e",
+    "url": "/static/css/main.0972467d.chunk.css"
+  },
+  {
+    "revision": "6f7c29589051ca9b7bd5",
+    "url": "/static/js/2.423833a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.423833a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7a733d0f1dad4faa8e",
+    "url": "/static/js/main.b84085f7.chunk.js"
+  },
+  {
+    "revision": "135330ec4a64660687a0",
+    "url": "/static/js/runtime-main.d2780e72.js"
+  }
+]);
